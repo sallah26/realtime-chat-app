04436a481e771d78d5c4c3e837f051ace46a4d4a
sallah26 <143meski@gmail.com>
2024-02-29 16:07:20 +0300

    initializing the project

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyDwGWdoiz0Fcs8OJSufVY3K6YL4ih1JPrc",
+  authDomain: "blog-posting-75b09.firebaseapp.com",
+  projectId: "blog-posting-75b09",
+  storageBucket: "blog-posting-75b09.appspot.com",
+  messagingSenderId: "1094993720306",
+  appId: "1:1094993720306:web:50f8725b223766b835b16b",
+  measurementId: "G-78R9BXFK3S"
+};
+
+const app = initializeApp(firebaseConfig);
